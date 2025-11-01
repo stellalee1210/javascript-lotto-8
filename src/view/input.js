d1edc63e5fb2503c8d55aa63b43db9b68ff7187b
stellalee1210 <yeobinlee1210@gmail.com>
2025-11-01 21:23:44 +0900

    fix(input): 입력값 한 번에 받아지는 오류 수정
    - async/await 추가

--- src/view/input.js
+++ src/view/input.js
@@ -3,15 +3,12 @@ import {
   PURCHASE_AMOUT_MESSAGE,
   WINNING_NUMBER_MESSAGE,
   BONUS_NUMBER_MESSAGE,
-} from "../constants/messages";
-export const readPurchaseAmount = await Console.readLineAsync(
-  PURCHASE_AMOUT_MESSAGE
-); //공백 입력해도 에러 안 남
+} from "../constants/messages.js";
+export const readPurchaseAmount = async () =>
+  await Console.readLineAsync(PURCHASE_AMOUT_MESSAGE); //공백 입력해도 에러 안 남
 
-export const readWinningNumber = await Console.readLineAsync(
-  WINNING_NUMBER_MESSAGE
-);
+export const readWinningNumber = async () =>
+  await Console.readLineAsync(WINNING_NUMBER_MESSAGE);
 
-export const readBonusNumber = await Console.readLineAsync(
-  BONUS_NUMBER_MESSAGE
-);
+export const readBonusNumber = async () =>
+  await Console.readLineAsync(BONUS_NUMBER_MESSAGE);

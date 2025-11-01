979f68c7b43e10a9dce547b6e0a0d8e6ac0e9452
stellalee1210 <yeobinlee1210@gmail.com>
2025-11-01 20:50:44 +0900

    refactor(LottoSimulator): 구입 금액 입력값 로직 수정

--- src/LottoSimulator.js
+++ src/LottoSimulator.js
@@ -1,16 +1,22 @@
 import { readPurchaseAmount } from "./view/input.js";
 import { Validator } from "./utils/Validator.js";
+import { LottoGame } from "./model/LottoGame.js";
 
 export class LottoSimulator {
   #purchaseAmount;
   constructor() {
-    this.#purchaseAmount = this.getPurchaseAmount();
+    this.#startLotto();
+  }
+
+  async #startLotto() {
+    await this.getPurchaseAmount();
   }
 
   async getPurchaseAmount() {
     try {
       const purchaseAmount = await readPurchaseAmount(); //구입 금액 입력 읽어오기
       Validator.validatePurchaseAmount(purchaseAmount);
+      this.#purchaseAmount = purchaseAmount;
     } catch (error) {
       throw Error(error);
     }

2b1e5d142e905298c6be0ff6f7faf2a509d0b009
stellalee1210 <yeobinlee1210@gmail.com>
2025-11-02 23:36:27 +0900

    test(LottoGame): 테스트 로직 수정

--- src/model/LottoGame.test.js
+++ src/model/LottoGame.test.js
@@ -1,4 +1,5 @@
 import { LottoGame } from "./LottoGame.js";
+import { Lotto } from "../Lotto.js";
 describe("LottoGame 클래스 기능 단위 테스트", () => {
   test("입력한 구입 금액만큼 로또 번호 자동 생성", () => {
     const moneyInput = 5000;
@@ -8,25 +9,31 @@ describe("LottoGame 클래스 기능 단위 테스트", () => {
 
   test("당첨된 값 확인", () => {
     const randomNum = [
-      "[8, 21, 23, 41, 42, 43]",
-      "[3, 5, 11, 16, 32, 38]",
-      "[7, 11, 16, 35, 36, 44]",
-      "[1, 8, 11, 31, 41, 42]",
-      "[13, 14, 16, 38, 42, 45]",
-      "[7, 11, 30, 40, 42, 43]",
-      "[2, 13, 22, 32, 38, 45]",
-      "[1, 3, 5, 14, 22, 45]",
+      [8, 21, 23, 41, 42, 43],
+      [3, 5, 11, 16, 32, 38],
+      [7, 11, 16, 35, 36, 44],
+      [1, 8, 11, 31, 41, 42],
+      [13, 14, 16, 38, 42, 45],
+      [7, 11, 30, 40, 42, 43],
+      [2, 13, 22, 32, 38, 45],
+      [1, 3, 5, 14, 22, 45],
     ];
-    const winningNumber = ["1,2,3,4,5,6", "7"];
+    const lottoTickets = randomNum.map((v) => new Lotto(v));
+    const winningNumber = [1, 2, 3, 4, 5, 6];
+    const bonusNumber = 7;
     const expectReturnValue = [1, 0, 0, 0, 0];
-    const returnValue = lottoGame.checkWinningNumber(randomNum, winningNumber);
+    const returnValue = LottoGame.calculateWinningNumber(
+      lottoTickets,
+      winningNumber,
+      bonusNumber
+    );
     expect(returnValue).toEqual(expectReturnValue);
   });
   test("수익률 계산", () => {
     const payAmount = 8000;
     const profit = 5000;
     const profitRate = (profit / payAmount) * 100;
-    const returnValue = lottoGame.calculateProfitRate(profit, payAmount);
+    const returnValue = LottoGame.calculateProfitRate(profit, payAmount);
     expect(returnValue).toEqual(profitRate);
   });
 });

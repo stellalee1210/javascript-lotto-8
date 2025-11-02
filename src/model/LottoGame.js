4bbde2c7b379c839638b11e7f6719f3e6b3c0128
stellalee1210 <yeobinlee1210@gmail.com>
2025-11-03 04:25:19 +0900

    fix(LottoGame): 당첨 금액 비교 로직 수정

--- src/model/LottoGame.js
+++ src/model/LottoGame.js
@@ -7,7 +7,6 @@ import {
 } from "../constants/constants.js";
 import { Lotto } from "../Lotto.js";
 import { PRIZE_AMOUNT } from "../constants/constants.js";
-import { Console } from "@woowacourse/mission-utils";
 
 export class LottoGame {
   static generateLottoNumbers(amount) {
@@ -31,22 +30,26 @@ export class LottoGame {
   }
 
   static calculateWinningNumber(lottoTickets, winningNumber, bonusNumber) {
-    const finalWinningNumber = [...winningNumber, bonusNumber]; //당첨 번호와 보너스 번호를 합침
-    const winningNumAndBonusNumSet = new Set(finalWinningNumber);
-    const result = new Array(5).fill(0);
+    const winningNumSet = new Set(winningNumber);
+    let result = new Array(5).fill(0);
 
     for (const ticket of lottoTickets) {
-      let bonusNumberState = 0;
       const matching = ticket.lottoNumbers.filter((num) => {
-        return winningNumAndBonusNumSet.has(num);
+        return winningNumSet.has(num);
       });
-
       const matchingCount = matching.length;
       if (matchingCount < MIN_MATCHING_VALUE) continue;
-      if (matchingCount === 6 && !ticket.lottoNumbers.includes(bonusNumber))
-        bonusNumberState++;
 
-      const index = matchingCount - MIN_MATCHING_VALUE + bonusNumberState;
+      let index = matchingCount - MIN_MATCHING_VALUE;
+      if (matchingCount === 6) {
+        result[5]++;
+        continue;
+      }
+      if (matchingCount === 5 && ticket.lottoNumbers.includes(bonusNumber)) {
+        result[4]++;
+        continue;
+      }
+
       result[index]++;
     }
 
@@ -54,9 +57,10 @@ export class LottoGame {
   }
 
   static calculateProfitRate(matchResult, purchaseAmount) {
-    const totalExpense = purchaseAmount * 1000;
+    const totalExpense = Number(purchaseAmount);
     const resultToMoney = matchResult.map((v, i) => v * PRIZE_AMOUNT[i]);
     const totalProfit = resultToMoney.reduce((acc, cur) => acc + cur);
+    if (totalProfit === 0) return 0;
 
     const profitRate = Number(((totalProfit / totalExpense) * 100).toFixed(1));
     return profitRate;
